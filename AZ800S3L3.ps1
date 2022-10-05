8fe9c268f1d20fddd35e66449f5c78fb0bfa2c23
broehmguru <75449846+broehmguru@users.noreply.github.com>
2022-10-05 18:30:40 -0500

    Update AZ800S3L3.ps1

--- AZ800S3L3.ps1
+++ AZ800S3L3.ps1
@@ -1,5 +1,3 @@
-# Run this script on a Windows PC (or Mac without | clip, to generate the encoded command for the script resource)
-$Text = @'
 $ProgressPreference = "SilentlyContinue"
 $WarningPreference = "SilentlyContinue"
 Get-ScheduledTask -TaskName ServerManager | Disable-ScheduledTask -Verbose
@@ -9,7 +7,3 @@ Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies
 Install-WindowsFeature "AD-Domain-Services" -IncludeManagementTools | Out-Null
 $pw = ConvertTo-SecureString "p@55w0rd" -AsPlainText -Force
 Install-ADDSForest -DomainName "corp.barrierreefaudio.com" -SafeModeAdministratorPassword $pw -DomainNetBIOSName 'CORP' -InstallDns -Force
-'@
-$Bytes = [System.Text.Encoding]::Unicode.GetBytes($Text)
-$EncodedText =[Convert]::ToBase64String($Bytes)
-$EncodedText
